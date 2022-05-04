c20a9cf01249eceeb7c2b8b93e8c4a2e3223652e
Ghostwolf73 <solomzigumbo@gmail.com>
2022-05-04 12:07:14 +0200

    App now displays the weather, description and icon

--- app.js
+++ app.js
@@ -5,18 +5,23 @@ const app = express();
 
 app.get("/", function(req, res){
 
-    let url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=aea1059100409efea41bd792327cd1f6&units=m"
+    let url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=aea1059100409efea41bd792327cd1f6&units=metric"
     https.get(url, function(response){
         console.log(response.statusCode);
 
         response.on("data", function(data){
-            const weatherData = JSON.parse(data)
-            const temp = weatherData.weather[0].description
-
-            console.log(temp);
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.main.temp;
+            const weatherDescr = weatherData.weather[0].description;
+            const weatherIcon = weatherData.weather[0].icon;
+            const imgURL = `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`
+            res.write(`<h1>The weather is ${temp} degrees`);
+            res.write(`<h2>${weatherDescr}</h2>`);
+            res.write("<img src="+ imgURL +">");
+            res.send();
         })
     });
-    res.send("Server is up and running!");
+    
 });
 
 
